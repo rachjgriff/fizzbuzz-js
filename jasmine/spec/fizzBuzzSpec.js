7900a1f1ff25dd13e79483cce2eec486e729ad68
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-25 15:00:58 +0100

    prints Fizz instead of 3 or 6

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -2,23 +2,27 @@ describe("Fizzbuzz", function() {
 
   var fizzBuzz;
 
-  it("prints number 1", function() {
+  beforeEach(function() {
     fizzBuzz = new FizzBuzz();
+  });
+
+  it("prints number 1", function() {
     expect(fizzBuzz.play(1)).toEqual(1);
   });
 
   it("prints number 2", function() {
-    fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.play(2)).toEqual(2);
   });
 
   it("prints Fizz instead of 3", function() {
-    fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.play(3)).toEqual('Fizz');
   });
 
   it("prints Buzz instead of 5", function() {
-    fizzBuzz = new FizzBuzz();
     expect(fizzBuzz.play(5)).toEqual('Buzz');
+  });
+
+  it("prints Fizz instead of 6", function() {
+    expect(fizzBuzz.play(6)).toEqual('Fizz');
   });
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -3,7 +3,7 @@ function FizzBuzz() {
 };
 
 FizzBuzz.prototype.play = function(number) {
-  if (number === 3) {
+  if (number === 3 || number === 6) {
     return 'Fizz';
   } else if (number === 5) {
     return 'Buzz';
