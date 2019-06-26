6b6b576b43a6f36ffed6955f43ecb2b9483d75e1
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-26 09:59:34 +0100

    Refactored Divisble By & Add Print Numbers function

--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,15 +1,24 @@
 function FizzBuzz() {
-
-};
+}
 
 FizzBuzz.prototype.play = function(number) {
-  if (number % 15 === 0) {
+  if (this._isDivisibleBy(number, 15)) {
     return 'FizzBuzz';
-  } else if (number % 3 === 0) {
+  } else if (this._isDivisibleBy(number, 3)) {
     return 'Fizz';
-  } else if (number % 5 === 0) {
+  } else if (this._isDivisibleBy(number, 5)) {
     return 'Buzz';
   } else {
     return number;
-  };
-};
+  }
+}
+
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return number % divisor === 0;
+}
+
+var fizzBuzz = new FizzBuzz();
+
+for (var i = 1; i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+}
